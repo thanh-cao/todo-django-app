8f6b60a3a4ca4d476a3d10807befdec057d46fa2
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-03-13 14:18:11 +0100

    set up Item model and migrattion and adjust todo list template to display todos

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    done = models.BooleanField(default=False, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 # Create your views here.
+
+
 def get_todo_list(req):
-    return render(req, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(req, 'todo/todo_list.html', context)
