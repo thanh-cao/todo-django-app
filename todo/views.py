047dcbcb97e79cbcc41a775308366e9b2628bb76
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-03-13 15:00:10 +0100

    add view to delete and toggle done

--- todo/views.py
+++ todo/views.py
@@ -4,12 +4,12 @@ from .forms import ItemForm
 # Create your views here.
 
 
-def get_todo_list(req):
+def get_todo_list(request):
     items = Item.objects.all()
     context = {
         'items': items
     }
-    return render(req, 'todo/todo_list.html', context)
+    return render(request, 'todo/todo_list.html', context)
 
 
 def add_item(request):
@@ -37,3 +37,16 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
