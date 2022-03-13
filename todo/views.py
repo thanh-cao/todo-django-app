a657794333b3e8c8e2a6530af90ce9805a722861
Thanh <55603278+thanh-cao@users.noreply.github.com>
2022-03-13 14:49:34 +0100

    add function and template to create new todo

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 # Create your views here.
 
@@ -9,3 +9,12 @@ def get_todo_list(req):
         'items': items
     }
     return render(req, 'todo/todo_list.html', context)
+
+
+def add_item(req):
+    if req.method == 'POST':
+        name = req.POST['item_name']
+        done = 'item_done' in req.POST
+        Item.objects.create(name=name, done=done)
+        return redirect('get_todo_list')
+    return render(req, 'todo/add_item.html')
